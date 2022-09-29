e175d68cfd91f3ba5315e3b26eb85fdab214d88f
Rohit Rozario <rrozario958@gmail.com>
2022-09-30 02:11:19 +0530

    added layout management and created title

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Nav from "./components/Nav";
 import Home from "./pages/Home";
 import Starred from "./pages/Starred";
 
 function App() {
   return (
-    <div className="App">
-      <Router>
-        <Nav />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/starred" element={<Starred />} />
-          <Route path="*" element={<h1>404 Page not found</h1>} />
-        </Routes>
-      </Router>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/starred" element={<Starred />} />
+        <Route path="*" element={<h1>404 Page not found</h1>} />
+      </Routes>
+    </Router>
   );
 }
 
