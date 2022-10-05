d04c7b1b601c985b11ac5468ac4f0105fc035547
Rohit Rozario <rrozario958@gmail.com>
2022-10-05 21:30:11 +0530

    added show page

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
+import Show from "./pages/Show";
 import Starred from "./pages/Starred";
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/starred" element={<Starred />} />
+        <Route path="/show/:id" element={<Show />} />
         <Route path="*" element={<h1>404 Page not found</h1>} />
       </Routes>
     </Router>
